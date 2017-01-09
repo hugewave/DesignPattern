915f87d11834517be4fe85175092f02fe3a5164f
Leo Wang <superwanghb@gmail.com>
2017-01-10 07:02:41 +0800

    Update TestProxyPattern.cpp

--- TestProxyPattern.cpp
+++ TestProxyPattern.cpp
@@ -1,5 +1,5 @@
 /*
- * Example of 'proxy' design pattern.
+ * Example of 'Proxy' design pattern.
  * Copyright (C) 2016 Leo Wang
  *
  * This program is free software: you can redistribute it and/or modify
